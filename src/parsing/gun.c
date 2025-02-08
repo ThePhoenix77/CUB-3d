3bb45031f3d22c91f896e39b5e5f3b3862495823
Anas Ragragui <aragragu@e2r11p4.1337.ma>
2025-02-08 21:53:07 +0100

    gun animation

--- src/parsing/gun.c
+++ src/parsing/gun.c
@@ -6,7 +6,7 @@
 /*   By: aragragu <aragragu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 12:46:09 by aragragu          #+#    #+#             */
-/*   Updated: 2025/02/08 21:11:05 by aragragu         ###   ########.fr       */
+/*   Updated: 2025/02/08 21:42:47 by aragragu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,30 +57,83 @@ void    load_gun_frames(t_data  *data)
             // free_textures(data);
             my_perror(1, "load textures::failed\n");
         }
-        // data->game.gun[i]->add = mlx_get_data_addr(data->game.gun[i]->image, &data->game.gun[i]->bits_per_pixel, &data->game.gun[i]->line_length, &data->game.gun[i]->endian);
+        data->game.gun[i]->add = mlx_get_data_addr(data->game.gun[i]->image, &data->game.gun[i]->bits_per_pixel, &data->game.gun[i]->line_length, &data->game.gun[i]->endian);
         i++;
         gun_num++;
     }
     data->game.current_frame = 0;
-    data->game.frame_delay = 5;
+    data->game.frame_delay = 2;
     data->game.frame_counter = 0;
 }
 
 
-void    render_gun_sprite(t_data *data)
-{
-    int x;
-    int y;
-    t_game *game;
+// void    render_gun_sprite(t_data *data)
+// {
+//     int x;
+//     int y;
+//     t_game *game;
 
-    if (data->game.frame_counter >= data->game.frame_delay) 
+//     if (data->game.frame_counter >= data->game.frame_delay) 
+//     {
+//         data->game.current_frame = (data->game.current_frame + 1) % 43;
+//         data->game.frame_counter = 0;
+//     }
+//     data->game.frame_counter++;
+//     game = &data->game;
+//     x = (MAP_WIDTH - game->gun[game->current_frame]->width) / 2;
+//     y = MAP_HEIGHT - game->gun[game->current_frame]->height;
+//     mlx_put_image_to_window(data->mlx, data->win, game->gun[game->current_frame]->image, x, y);
+// }
+void render_gun_sprite(t_data *data)
+{
+    int         x, y;
+    t_game      *game = &data->game;
+    t_textures  *gun_frame;
+    int         bytes_per_pixel_src, bytes_per_pixel_dst;
+    int         i, j;
+    char        *src_row;
+    unsigned char *dst_row;
+
+    if (game->frame_counter >= game->frame_delay)
     {
-        data->game.current_frame = (data->game.current_frame + 1) % 43;
-        data->game.frame_counter = 0;
+        game->current_frame = (game->current_frame + 1) % 43;
+        game->frame_counter = 0;
     }
-    data->game.frame_counter++;
-    game = &data->game;
-    x = (MAP_WIDTH - game->gun[game->current_frame]->width) / 2;
-    y = MAP_HEIGHT - game->gun[game->current_frame]->height;
-    mlx_put_image_to_window(data->mlx, data->win, game->gun[game->current_frame]->image, x, y);
+    game->frame_counter++;
+    gun_frame = game->gun[game->current_frame];
+    if (!gun_frame || !gun_frame->add)
+        return;
+
+    x = (MAP_WIDTH - gun_frame->width) / 2;
+    y = MAP_HEIGHT - gun_frame->height;
+    bytes_per_pixel_src = gun_frame->bits_per_pixel / 8;
+    bytes_per_pixel_dst = data->img.bpp / 8;
+
+    j = 0;
+    while (j < gun_frame->height)
+    {
+        int dest_y = y + j;
+        if (dest_y >= 0 && dest_y < MAP_HEIGHT)
+        {
+            dst_row = (unsigned char *)data->img.data + dest_y * data->img.size_line;
+            src_row = gun_frame->add + j * gun_frame->line_length;
+            i = 0;
+            while (i < gun_frame->width)
+            {
+                int dest_x = x + i;
+                if (dest_x >= 0 && dest_x < MAP_WIDTH)
+                {
+                    int color = *(int *)(src_row + i * bytes_per_pixel_src);
+                    if (color != 0x000000)
+                    {
+                        *(int *)(dst_row + dest_x * bytes_per_pixel_dst) = color;
+                    }
+                }
+                i++;
+            }
+        }
+        j++;
+    }
+    mlx_put_image_to_window(data->mlx, data->win, data->img.img_ptr, 0, 0);
 }
+
